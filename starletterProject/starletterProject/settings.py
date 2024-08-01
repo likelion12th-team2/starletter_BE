da533302e1af657e2fbacd104eb3067d86598dd3
Minju Kim <mnat713@naver.com>
2024-08-01 21:10:30 +0900

    deploy: MySQL DB 연결

--- starletterProject/starletterProject/settings.py
+++ starletterProject/starletterProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os, json
+import os, json, pymysql
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,10 +139,21 @@ WSGI_APPLICATION = 'starletterProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'hackathon', 
+        'USER' : 'hackathon', # DB User
+        'PASSWORD' : 'hackathon', # Password
+        'HOST': '13.124.168.131',
+        'PORT': '3306', # 데이터베이스 포트
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        }
     }
 }
 
